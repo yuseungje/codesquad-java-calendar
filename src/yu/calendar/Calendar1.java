3fe2c3fe8ff39f1bdaa556bf61dda59225a63ee0
aachi <aachi@DESKTOP-4DDP1UO>
2018-03-29 18:27:08 +0900

    Implement Max Days

--- src/yu/calendar/Calendar1.java
+++ src/yu/calendar/Calendar1.java
@@ -1,5 +1,7 @@
 package yu.calendar;
 
+import java.util.Scanner;
+
 public class Calendar1 {
 	public static void main(String[] args) {
 		System.out.println("Hello!!!");
@@ -9,5 +11,17 @@ public class Calendar1 {
 		System.out.println("  8  9 10 11 12 13 14 ");
 		System.out.println(" 15 16 17 18 19 20 21 ");
 		System.out.println(" 22 23 24 25 26 27 28 ");
+		
+		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("달을 입력하세요.");
+		int month = scanner.nextInt();
+		
+		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		
+		System.out.printf("%d월은 %d일까지 있습니다. \n", month,maxDays[month-1]);
+				
+		scanner.close();
+		
 	}
 }
